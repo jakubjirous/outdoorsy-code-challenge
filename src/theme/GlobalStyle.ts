4cc23bcedb07399e8e114dce9f7de85ab7e95517
Jakub Jirous <info@jakubjirous.cz>
2022-05-10 15:24:08 -0700

    [theme] color palette

--- src/theme/GlobalStyle.ts
+++ src/theme/GlobalStyle.ts
@@ -1,8 +1,10 @@
 import { createGlobalStyle } from "styled-components";
+import { Palette } from "./Palette";
 import { Reset } from "./Reset";
 import { Variables } from "./Variables";
 
 export const GlobalStyles = createGlobalStyle`
   ${Reset}
   ${Variables}
+  ${Palette}
 `;
---
+++ src/theme/Palette.ts
@@ -0,0 +1,19 @@
+import { css } from "styled-components";
+
+export const Palette = css`
+  :root {
+    // Colors
+    --atoll: #0a6c63;
+    --silver: #f7f7f7;
+    --gallery: #eeeeee;
+    --wild-sand: #f5f5f5;
+    --wisp: #fceaec;
+    --white: #ffffff;
+    --black: #000000;
+
+    // Color Palette
+    --color-primary: var(--atoll);
+    --color-secondary: var(--wild-sand);
+    --color-tertiary: var(--gallery);
+  }
+`;
