6a1caa2b5e0adc9a3c81c57bae8950d6c3b25e0b
Jakub Jirous <info@jakubjirous.cz>
2022-05-10 15:14:38 -0700

    [theme] create global style

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./components/App/App";
 import { store } from "./redux/store";
+import { GlobalStyles } from "./theme/GlobalStyle";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <GlobalStyles />
       <App />
     </Provider>
   </React.StrictMode>
---
+++ src/theme/GlobalStyle.ts
@@ -0,0 +1,6 @@
+import { createGlobalStyle } from "styled-components";
+import { Reset } from "./Reset";
+
+export const GlobalStyles = createGlobalStyle`
+  ${Reset}
+`;
